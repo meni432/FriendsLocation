128d8fcb8b81aea337c61e20e46bba0d503bc8fd
Meni Samet <meni432@gmail.com>
2016-12-05 00:34:41 +0200

    insert google sign in

---
+++ app/src/main/java/com/menisamet/friendslocation/Utility.java
@@ -0,0 +1,30 @@
+package com.menisamet.friendslocation;
+
+import android.content.Context;
+import android.content.Intent;
+
+/**
+ * Created by meni on 05/12/16.
+ */
+
+public class Utility {
+    public static final String EXTRA_NEXT_ACTIVITY = "extra_next_activity";
+    public static final String EXTRA_LOG_OUT = "extra_log_out";
+
+
+    public static void checkAuthAndGoToActivity(Context context, Class nextActivity) {
+        Intent intent = new Intent(context, GoogleSignInActivity.class);
+        intent.putExtra(EXTRA_NEXT_ACTIVITY, nextActivity);
+        context.startActivity(intent);
+//        Intent intent = new Intent(context, nextActivity);
+//        context.startActivity(intent)
+
+    }
+
+    public static void logOutUser(Context context) {
+        Intent intent = new Intent(context, GoogleSignInActivity.class);
+        intent.putExtra(EXTRA_NEXT_ACTIVITY, MainActivity.class);
+        intent.putExtra(EXTRA_LOG_OUT, true);
+        context.startActivity(intent);
+    }
+}
