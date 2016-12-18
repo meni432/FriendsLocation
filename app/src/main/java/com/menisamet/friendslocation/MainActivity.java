d404b385e9323fddc655ed5f1563979021e1cc63
moshon89 <moshon89@gmail.com>
2016-12-18 12:33:07 +0200

    barcode

--- app/src/main/java/com/menisamet/friendslocation/MainActivity.java
+++ app/src/main/java/com/menisamet/friendslocation/MainActivity.java
@@ -1,7 +1,12 @@
 package com.menisamet.friendslocation;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        IntentIntegrator integrator = new IntentIntegrator(this);
+        integrator.initiateScan();
+    }
+
+    public void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        IntentResult scanResult = IntentIntegrator.parseActivityResult(requestCode, resultCode, intent);
+        if (scanResult != null) {
+            Log.d("tag", scanResult.getContents());
+        }
+        // else continue with any other code you need in the method
+
     }
 }
