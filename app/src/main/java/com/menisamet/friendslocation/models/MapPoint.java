1d36e2eae096ddc5d0426c914b02a23fa933d187
Meni Samet <meni432@gmail.com>
2016-12-20 22:07:20 +0200

    start working on database

--- app/src/main/java/com/menisamet/friendslocation/models/MapPoint.java
+++ app/src/main/java/com/menisamet/friendslocation/models/MapPoint.java
@@ -1,8 +1,10 @@
 package com.menisamet.friendslocation.models;
 
-import android.location.Location;
+import com.google.firebase.database.Exclude;
 
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by meni on 19/12/16.
@@ -12,18 +14,24 @@ public class MapPoint {
 
     private UserDetails mUserDetailsOwner;
     private Date mDate;
-    private String mNodeTag;
+    private String mTag;
+    private MapLocation mMapLocation;
+    private String dbKey;
 
+
+    @Exclude
+    public Map<String, Object> toMap() {
+        HashMap<String, Object> result = new HashMap<>();
+        result.put("UserDetails", mUserDetailsOwner);
+        result.put("Date", mDate);
+        result.put("Tag", mTag);
+        result.put("MapLocation", mMapLocation);
+
+        return result;
+    }
     public MapPoint() {
     }
 
-    public void setTag(String tag) {
-        this.mNodeTag = tag;
-    }
-
-    public String getTag() {
-        return mNodeTag;
-    }
     public UserDetails getUserDetailsOwner() {
         return mUserDetailsOwner;
     }
@@ -32,13 +40,35 @@ public class MapPoint {
         this.mUserDetailsOwner = mUserDetailsOwner;
     }
 
-
-
     public Date getDate() {
         return mDate;
     }
 
-    public void setDate(Date mDate) {
+    public void setmDate(Date mDate) {
         this.mDate = mDate;
+    }
+
+    public String getmTag() {
+        return mTag;
+    }
+
+    public void setTag(String mTag) {
+        this.mTag = mTag;
+    }
+
+    public MapLocation getMapLocation() {
+        return mMapLocation;
+    }
+
+    public void setMapLocation(MapLocation mMapLocation) {
+        this.mMapLocation = mMapLocation;
+    }
+
+    public String getDbKey() {
+        return dbKey;
+    }
+
+    public void setDbKey(String dbKey) {
+        this.dbKey = dbKey;
     }
 }
